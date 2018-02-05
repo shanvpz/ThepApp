6e20fd6d30884b23ced53c5925a3a94a254a51ec
shankm <shankm2010@gmail.com>
2018-02-05 16:25:20 +0530

    thepstories from utube added fragment

---
+++ app/src/main/java/in/techfantasy/thepapp/ThepStories.java
@@ -0,0 +1,124 @@
+package in.techfantasy.thepapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ThepStories.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ThepStories#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ThepStories extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public ThepStories() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ThepStories.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ThepStories newInstance(String param1, String param2) {
+        ThepStories fragment = new ThepStories();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_thep_stories, container, false);
+        WebView wv=v.findViewById(R.id.webview1);
+        wv.getSettings().setLoadWithOverviewMode(true);
+        wv.getSettings().setUseWideViewPort(true);
+        wv.setWebViewClient(new WebViewClient());
+        final String mimeType = "text/html";
+        final String encoding = "UTF-8";
+        wv.getSettings().setJavaScriptEnabled(true);
+        String html = "<style>.meta{display:none} .html5-video-player{}</style>"+
+                "<div style=\"width:100%\"><script src=\"http://www.yvoschaap.com/ytpage/ytembed.js\"></script><div id=\"ytThumbs\"></div></div><script> ytEmbed.init({'block':'ytThumbs','key':'AIzaSyCZEzlbeZgHKnn97NUNIRJ2IvBDS97mQIc','q':'PLS4UjDrkf-h3EDnuXRZpLSSVk2C5p_no2','type':'playlist','results':5,'meta':true,'player':'embed','layout':'full'});</script>";
+
+
+        wv.loadDataWithBaseURL("", html, mimeType, encoding, "");
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
