58d11e0189b177f5a4079d502985892b4857b600
jobinjnj <9847116011.zion>
2018-02-08 14:52:19 +0530

    send feedback Completed 02

--- app/src/main/java/in/techfantasy/thepapp/FeedBackFragment.java
+++ app/src/main/java/in/techfantasy/thepapp/FeedBackFragment.java
@@ -1,12 +1,20 @@
 package in.techfantasy.thepapp;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -22,6 +30,9 @@ public class FeedBackFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    EditText etxmsg,etxname;
+    Button btnsnd;
+    Boolean check=false;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,10 +72,60 @@ public class FeedBackFragment extends Fragment {
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if(resultCode==RESULT_OK&&requestCode==123) {
+            if (check) {
+                Toast.makeText(getActivity(), "FeedBack Send Successfully", Toast.LENGTH_SHORT).show();
+                etxname.getText().clear();
+                etxmsg.getText().clear();
+                loadContent(new HomeFragment());
+
+            }
+        }else{
+            Toast.makeText(getActivity(), "Sending failed Try again later", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v=inflater.inflate(R.layout.fragment_feed_back, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed_back, container, false);
+        etxmsg=v.findViewById(R.id.feedmsg);
+        etxname=v.findViewById(R.id.feedname);
+        btnsnd=v.findViewById(R.id.feedsend);
+        btnsnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nam,msg;
+                nam=etxname.getText().toString();
+                msg=etxmsg.getText().toString();
+
+                if(nam.equals("")||msg.equals("")){
+                    Toast.makeText(getActivity(),"All fields are mandatory!",Toast.LENGTH_SHORT).show();
+                }
+
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"appthepp@gmail.com"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "FeedBack - Thepp App - "+nam);
+                i.putExtra(Intent.EXTRA_TEXT   , msg);
+                try {
+                    startActivityForResult(Intent.createChooser(i, "Send mail..."),123);
+                    //startActivity(Intent.createChooser(i, "Send mail..."));
+                    check=true;
+
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getActivity(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -104,5 +165,12 @@ public class FeedBackFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private void loadContent(Fragment f){
+        FragmentManager fm=getFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        ft.replace(R.id.frameLayout,f);
+        ft.commit();
     }
 }
--- app/src/main/java/in/techfantasy/thepapp/Main2Activity.java
+++ app/src/main/java/in/techfantasy/thepapp/Main2Activity.java
@@ -137,9 +137,10 @@ public class Main2Activity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
